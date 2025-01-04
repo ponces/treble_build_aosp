3acb7657fbfee18e90745afa7ece51caf8d095b7
Alberto Ponces <ponces26@gmail.com>
2025-01-04 16:11:09 +0000

    bump: up to android-15.0.0_r9

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ BV=$1
 
 initRepos() {
     echo "--> Initializing workspace"
-    repo init -u https://android.googlesource.com/platform/manifest -b android-15.0.0_r8 --git-lfs
+    repo init -u https://android.googlesource.com/platform/manifest -b android-15.0.0_r9 --git-lfs
     echo
 
     echo "--> Preparing local manifest"
